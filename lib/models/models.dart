07a141697b188be44df4c069ab364a96302566b3
prakash-15296 <prakash.vel@zohocorp.com>
2024-04-01 15:50:51 +0530

    initial commit

--- lib/models/models.dart
+++
@@ -1,7 +0,0 @@
-class ModuleEntity {
-    const ModuleEntity({required this.name, required this.desc, required this.moduleInvoker});
-
-    final String name;
-    final String desc;
-    final Function moduleInvoker;
-}
